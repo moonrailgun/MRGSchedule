17d8ca57602981a72840beab8812bee30dd2a0a4
moonrailgun <moonrailgun@gmail.com>
2015-09-09 16:54:54 +0800

    课程信息数据结构
    ADD 课程信息类
    ADD 星期数枚举
    ADD 课程结构体
    ADD 课程表添加课程与课程表数据实例

---
+++ MRGSchedule/LessonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRGSchedule
+{
+    class LessonInfo
+    {
+        public string lessonName;
+        public int lessonStartWeek;
+        public int lessonOverWeek;
+        public string lessonTeacher;
+        public string lessonSite;
+        public List<string> lessonRemarks;
+    }
+}
---
+++ MRGSchedule/Schedule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRGSchedule
+{
+    struct Lesson
+    {
+        public WeekDay weekNum;//星期几
+        public int classNum;//第几大节
+        public LessonInfo lesson;
+    }
+    enum WeekDay
+    {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6
+    }
+
+    class Schedule
+    {
+        private List<Lesson> lessonList = new List<Lesson>();
+
+        public bool AddLesson(Lesson lesson)
+        {
+            foreach (Lesson l in lessonList)
+            {
+                if (l.classNum == lesson.classNum && l.weekNum == lesson.weekNum)
+                {
+                    return false;
+                }
+            }
+
+            lessonList.Add(lesson);
+            return true;
+        }
+    }
+}
