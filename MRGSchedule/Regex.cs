b54276321e4b1c184a7c9941428c022437d60c85
moonrailgun <moonrailgun@gmail.com>
2015-09-11 19:08:52 +0800

    两种数据匹配方式
    ADD 标签页数据获取
    ADD 正则匹配多种条件获取

--- MRGSchedule/Regex.cs
+++ MRGSchedule/Regex.cs
@@ -9,6 +9,9 @@ namespace MRGSchedule
 {
     class Regex
     {
+        /// <summary>
+        /// 获取两端文本中间的文本
+        /// </summary>
         public static string GetMiddleContent(string originStr, string startStr, string endStr, bool includeEdge = false)
         {
             string returnStr = "";
@@ -23,6 +26,42 @@ namespace MRGSchedule
                 {
                     returnStr = startStr + returnStr + endStr;
                 }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+            return returnStr;
+        }
+
+        /// <summary>
+        /// 根据正则表达式获取所有匹配的数据
+        /// </summary>
+        public static List<string> GetMatchedGroup(string originStr, string pattern)
+        {
+            List<string> returnStrs = new List<string>();
+            try
+            {
+                MatchCollection mats = System.Text.RegularExpressions.Regex.Matches(originStr, pattern);
+                foreach (Match mat in mats)
+                {
+                    returnStrs.Add(mat.Value.ToString());
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+            return returnStrs;
+        }
+
+        /// <summary>
+        /// 获取标签页中间的数据
+        /// </summary>
+        public static string GetInnerHtml(string originStr)
+        {
+            string regexStr = @">([^<]*)</";
+            string returnStr = "";
+            try
+            {
+                Match mat = System.Text.RegularExpressions.Regex.Match(originStr, regexStr);
+                returnStr = mat.Groups[0].Value.ToString();
             }
             catch (Exception ex) { Console.WriteLine(ex.ToString()); }
 
